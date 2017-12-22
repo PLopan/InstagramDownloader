fef4cf4fe383005d14a9b9468b29d69b87c19e23
Pablo Lopez <pablolopan@gmail.com>
2017-12-22 15:52:05 +0100

    Fixed error. Did not allow underscore in URL

--- src/insta_down.py
+++ src/insta_down.py
@@ -11,14 +11,14 @@ def get_fullres_photo_url(media_url):
     html = sock.read()
     soup = BeautifulSoup(html, "html.parser")
     meta_image_tag = soup.find_all("meta", {"property":"og:image"})[0]
-    fullres_url = meta_image_tag["content"]
+    fullres_url = meta_image_tag["content"]    
     return fullres_url
 
 def is_accepted_media_url(media_url):
     """
         Checks if the input url is an accepted instagram media url.
     """
-    url_regex = "https?://www.instagram.com/p/([a-zA-Z0-9]+)/?.*"
+    url_regex = "https?://www.instagram.com/p/([a-zA-Z0-9_]+)/?.*"
     return re.match(url_regex, media_url)
 
 def clean_media_url(media_url):
@@ -26,7 +26,7 @@ def clean_media_url(media_url):
         Gets the input url to its brief form. (http[s]://instagram.com/p/xxxxx)
         The param media_url needs to be checked with is_accepted_media_url before.
     """
-    brief_regex = "https?://www.instagram.com/p/([a-zA-Z0-9]+)"
+    brief_regex = "https?://www.instagram.com/p/([a-zA-Z0-9_]+)"
     matched_substr = re.search(brief_regex, media_url)
     return matched_substr.group()
 
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     # Check the input link
     if is_accepted_media_url(args.link):
-        media_url = clean_media_url(args.link)  
+        media_url = clean_media_url(args.link)
         # Download the image
         filename = media_url.rsplit('/')[-1]
         print("Downloading image: " + filename)
